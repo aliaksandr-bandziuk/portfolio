5901d1f4c42f74d80d4577bdfaa3cb91521bd0f3
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2021-09-01 16:20:07 +0300

    fix: uprgrade swiperjs to version 7

--- js/swiperSlider.js
+++ js/swiperSlider.js
@@ -1,9 +1,10 @@
 // Инициализируем Swiper
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper', {
   pagination: {
     el: '.projects-pagination',
     bulletClass: 'projects-bullet',
     bulletActiveClass: 'projects-bullet-active',
     clickable: true
   },
+  loop: true,
 });
