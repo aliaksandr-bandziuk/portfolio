6c7e76e5daca223f688794a7539c704fc3903bfe
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-06-05 16:20:53 +0200

    add files

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,133 @@
+'use client';
+
+import { FC, useState, useEffect } from 'react';
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { motion } from 'framer-motion'
+import styles from './Contact.module.scss';
+
+export type FormData = {
+  name: string;
+  phone: string;
+  email: string;
+  agreedToPolicy: boolean;
+};
+
+export interface ContactFormProps {
+  onFormSubmitSuccess?: () => void; // Функция обратного вызова для успешной отправки
+}
+
+const Contact: FC<ContactFormProps> = ({ onFormSubmitSuccess }) => {
+  const [message, setMessage] = useState<string | null>(null);
+  const [filled, setFilled] = useState({ name: false, phone: false, email: false });
+
+    useEffect(() => {
+    const interval = setInterval(() => {
+      ['name', 'phone', 'email'].forEach(field => {
+        const input = document.getElementById(field) as HTMLInputElement;
+        if (input && input.value) {
+          setFilled(f => ({ ...f, [field]: true }));
+        }
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFilled(prev => ({ ...prev, [name]: value.trim() !== '' }));
+  };
+
+  const initialValues: FormData = {
+    name: '',
+    phone: '',
+    email: '',
+    agreedToPolicy: false,
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Imię jest wymagane'),
+    phone: Yup.string().required('Telefon jest wymagany'),
+    email: Yup.string().email('Nieprawidłowy адрес email').required('Email jest wymagany'),
+    agreedToPolicy: Yup.boolean()
+      .required('Wymagana zgoda')
+      .oneOf([true], 'Wymagana jest zgoda na przetwarzanie danych osobowych'),
+  });
+
+  const onSubmit = async (values: FormData, { setSubmitting, resetForm }: FormikHelpers<FormData>) => {
+    setSubmitting(true);
+    try {
+      const response = await axios.post('/api/email', values);
+      if (response.data.message === 'Email sent') {
+        setMessage('I received your message and will contact you soon. Wait a moment :)');
+        resetForm({});
+        setFilled({ name: false, phone: false, email: false }); // Reset the filled state
+        setTimeout(() => {
+          onFormSubmitSuccess && onFormSubmitSuccess();
+        }, 5000);
+      } else {
+        throw new Error('Server responded with an error');
+      }
+    } catch (error) {
+      setMessage('An error occurred while submitting the form. Please try again');
+    } finally {
+      setSubmitting(false);
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    }
+  };
+
+
+  return (
+    <>
+      {message && (
+        <div className={styles.popup}>
+          {message}
+        </div>
+      )}
+      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+        {({ isSubmitting }) => (
+          <Form>
+            <div className={styles.inputWrapper}>
+              <label htmlFor='name' className={`${styles.label} ${filled.name ? styles.filled : ''}`}>Name</label>
+              <Field id='name' name='name' type='text' className={`${styles.inputField} w-full rounded-md`} onBlur={handleBlur} />
+              <ErrorMessage name='name' component='div' className={styles.error} />
+            </div>
+            <div className={styles.inputWrapper}>
+              <label htmlFor='phone' className={`${styles.label} ${filled.phone ? styles.filled : ''}`}>Phone</label>
+              <Field id='phone' name='phone' type='tel' className={`${styles.inputField} w-full rounded-md`} onBlur={handleBlur} />
+              <ErrorMessage name='phone' component='div' className={styles.error} />
+            </div>
+            <div className={styles.inputWrapper}>
+              <label htmlFor='email' className={`${styles.label} ${filled.email ? styles.filled : ''}`}>Email</label>
+              <Field id='email' name='email' type='email' className={`${styles.inputField} w-full rounded-md`} onBlur={handleBlur} />
+              <ErrorMessage name='email' component='div' className={styles.error} />
+            </div>
+            <div className={styles.customCheckbox}>
+              <Field type="checkbox" name="agreedToPolicy" id="agreedToPolicy" />
+              <label htmlFor="agreedToPolicy">I agree to the processing of personal data</label>
+            </div>
+            <div>
+              {/* <button type='submit' className={styles.sentBtn} disabled={isSubmitting}>Wyślij wiadomość</button> */}
+              <motion.button
+                type='submit'
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ type: "spring", stiffness: 100, damping: 10 }}
+                className={styles.button}
+                disabled={isSubmitting}
+              >
+                Send
+              </motion.button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/components/ContactSection/ContactSection.tsx
@@ -0,0 +1,35 @@
+import Contact from '../Contact/Contact';
+import styles from './ContactSection.module.scss';
+import React from 'react';
+
+export const ContactSection = () => {
+  return (
+    <section className={styles.contact} id="contact">
+      <div className="container">
+        <div className={styles.contactInner}>
+          <div className={styles.contactWrapper}>
+            <div className={styles.contactInfo}>
+              <h2 className={styles.contactTitle}>Contact me</h2>
+              <div className={styles.divider}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="117" height="2" viewBox="0 0 117 2" fill="none">
+                  <path d="M0 1H116.5" stroke="url(#paint0_linear_16_101)" />
+                  <defs>
+                    <linearGradient id="paint0_linear_16_101" x1="-1.10394" y1="1.00004" x2="-1.09485" y2="-0.0666686" gradientUnits="userSpaceOnUse">
+                      <stop stopColor="#343045" />
+                      <stop stopColor="#343045" />
+                      <stop offset="0.348958" stopColor="#C0B7E8" />
+                      <stop offset="0.6875" stopColor="#8176AF" />
+                      <stop offset="1" stopColor="#343045" />
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+              <h3 className={styles.contactSubtitle}>Let&apos;s create a cool project for you</h3>
+            </div>
+            <Contact />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
