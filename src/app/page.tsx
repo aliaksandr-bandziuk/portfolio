ff3e85941c04cb3c3eb7d135ab56993626714e36
Aliaksandr Bandziuk <aliaksandr.bandziuk@gmail.com>
2025-01-08 23:18:02 +0100

    add portfolio section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { ContactSection } from "@/components/ContactSection/ContactSection";
-
+import Portfolio from "@/components/Portfolio/Portfolio";
 
 export default function Home() {
   return (
     <main>
       <ContactSection />
+      <Portfolio />
     </main>
   );
 }
---
+++ src/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styles from "./Portfolio.module.scss";
+import Link from "next/link";
+
+const Portfolio = () => {
+  return (
+    <section className={styles.portfolio}>
+      <div className="containerCustom">
+        <div className={styles.portfolioInner}>
+          <h2 className={styles.title}>Portfolio</h2>
+          <div className={styles.portfolioItems}>
+            <Link
+              href="https://academgo.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.portfolioItem}
+            >
+              Education
+            </Link>
+            <Link
+              href="https://landi-academy.pl"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.portfolioItem}
+            >
+              Beauty
+            </Link>
+            <Link
+              href="https://peakprofit.bandziuk.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.portfolioItem}
+            >
+              Blockchain
+            </Link>
+            <Link
+              href="https://proscore.agency"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.portfolioItem}
+            >
+              Marketing
+            </Link>
+            <Link
+              href="https://www.sashadith.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.portfolioItem}
+            >
+              DJ's website
+            </Link>
+            <Link
+              href="https://scanm2.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.portfolioItem}
+            >
+              Architecture
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
